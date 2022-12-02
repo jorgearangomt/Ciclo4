a1c7f5a65b686f293f4d0faec13a4598f110ee6d
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-12-01 21:08:20 -0500

    Add mongoose module and config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,23 @@
-const express = require('express');
-const v1SportRouter = require('./v1/routes/sportRoutes');
 require('dotenv').config()
+
+const express = require('express');
+
+const mongoose = require('mongoose');
+const mongoString = process.env.DEV_DATABASE_URL;
+
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error',(error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const v1SportRouter = require('./v1/routes/sportRoutes');
 
 const app = express();
 const PORT = process.env.DEV_PORT || 9000;
